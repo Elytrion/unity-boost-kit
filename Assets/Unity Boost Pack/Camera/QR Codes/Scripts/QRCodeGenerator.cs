b49a800b0ebe80d02482df9c1f357654c5de48d6
Jed Goh Yujie <electronfirework@gmail.com>
2019-02-27 16:34:04 +0800

    Implemented QRCodeScanner and QRCodeGenerator scripts

---
+++ Assets/Unity Boost Pack/Camera/QR Codes/Scripts/QRCodeGenerator.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using ZXing;
+using ZXing.QrCode;
+
+public static class QRCodeGenerator
+{
+    public static Sprite GenerateQRCode(string textToBeEncoded, int textureSize = 256)
+    {
+        Texture2D qrCode = new Texture2D(textureSize, textureSize);
+        Color32[] qrEncodedTexture = EncodeString(textToBeEncoded, qrCode.width, qrCode.height);
+
+        qrCode.SetPixels32(qrEncodedTexture);
+        qrCode.Apply();
+        return Sprite.Create(qrCode, new Rect(0, 0, qrCode.width, qrCode.height), new Vector2(0.5f, 0.5f));
+    }
+
+    private static Color32[] EncodeString(string textForEncoding, int width, int height)
+    {
+        var writer = new BarcodeWriter
+        {
+            Format = BarcodeFormat.QR_CODE,
+            Options = new QrCodeEncodingOptions
+            {
+                Height = height,
+                Width = width
+            }
+        };
+        return writer.Write(textForEncoding);
+    }
+
+}
---
+++ Assets/Unity Boost Pack/Camera/QR Codes/Scripts/QRCodeScanner.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using UnityEngine;
+using ZXing;
+
+public static class QRCodeScanner
+{
+    private static WebCamTexture _camTexture;
+
+    public static void StartWebCamera()
+    {
+        _camTexture = GetBackFacingWebCamTexture();
+        if (_camTexture == null)
+        {
+            _camTexture = GetFirstWebCamTexture();
+        }
+        if (_camTexture != null)
+        {
+            _camTexture.Play();
+        }
+    }
+
+    public static void StopWebCamera()
+    {
+        if (_camTexture.isPlaying)
+        {
+            _camTexture.Stop();
+        }
+    }
+
+    public static WebCamTexture GetWebCameraFeed()
+    {
+        if (_camTexture != null)
+        {
+            return _camTexture;
+        }
+        else
+        {
+            return null;
+        }
+    }
+
+    public static string DecodeCurrentFrame()
+    {
+        if (_camTexture != null)
+        {
+            try
+            {
+                IBarcodeReader qrCodeReader = new BarcodeReader();
+
+                var result = qrCodeReader.Decode(_camTexture.GetPixels32(), _camTexture.width, _camTexture.height);
+                if (result != null)
+                {
+                    return result.Text;
+                }
+                else
+                {
+                    return null;
+                }
+
+            }
+            catch (Exception ex) { Debug.LogWarning(ex.Message); return null; }
+        }
+        else
+        {
+            return null;
+        }
+    }
+
+    private static WebCamTexture GetBackFacingWebCamTexture()
+    {
+        foreach (WebCamDevice device in WebCamTexture.devices)
+        {
+            if (!device.isFrontFacing)
+            {
+                return new WebCamTexture(device.name);
+            }
+        }
+
+        return null;
+    }
+
+    private static WebCamTexture GetFirstWebCamTexture()
+    {
+        if (WebCamTexture.devices.Length > 0)
+        {
+            return new WebCamTexture(WebCamTexture.devices[0].name);
+        }
+        else
+        {
+            return null;
+        }
+    }
+
+}
