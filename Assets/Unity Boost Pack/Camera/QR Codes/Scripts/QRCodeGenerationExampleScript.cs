4341f66284c9b9be60617f4e9399755828eeb0b5
Jed Goh Yujie <electronfirework@gmail.com>
2019-02-27 16:35:00 +0800

    Implemented QRCodeGeneration and Scanning example scripts to show all the functions of the QRCodeScanner and Generator scripts

---
+++ Assets/Unity Boost Pack/Camera/QR Codes/Scripts/QRCodeGenerationExampleScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class QRCodeGenerationExampleScript : MonoBehaviour
+{
+    [SerializeField] private string _stringToEncode = string.Empty;
+    private Image _displayQRImage;
+
+    private void Awake()
+    {
+        _displayQRImage = GetComponent<Image>();
+    }
+
+    private void Start()
+    {
+        TryCreateQRCode();
+    }
+
+    private void TryCreateQRCode()
+    {
+        if (_stringToEncode.Length > 0)
+        {
+            CreateQRCode();
+        }
+    }
+
+    private void CreateQRCode()
+    {
+        _displayQRImage.sprite = QRCodeGenerator.GenerateQRCode(_stringToEncode);
+    }
+}
---
+++ Assets/Unity Boost Pack/Camera/QR Codes/Scripts/QRCodeScanningExampleScript.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class QRCodeScanningExampleScript : MonoBehaviour
+{
+    private RawImage _cameraDisplay;
+
+    private void Awake()
+    {
+        _cameraDisplay = GetComponent<RawImage>();
+    }
+
+    private void OnEnable()
+    {
+        QRCodeScanner.StartWebCamera();
+    }
+
+    private void OnDisable()
+    {
+        QRCodeScanner.StopWebCamera();
+    }
+
+    private void Update()
+    {
+        TryDecodeQRCode();
+    }
+
+    private void TryDecodeQRCode()
+    {
+        if (QRCodeScanner.GetWebCameraFeed().didUpdateThisFrame)
+        {
+            DisplayCamera();
+            DecodeQRCode();
+        }
+    }
+
+    private void DisplayCamera()
+    {
+        _cameraDisplay.texture = QRCodeScanner.GetWebCameraFeed();
+        _cameraDisplay.rectTransform.localEulerAngles = new Vector3(0, 0, -QRCodeScanner.GetWebCameraFeed().videoRotationAngle);
+    }
+
+    private void DecodeQRCode()
+    {
+        string result = QRCodeScanner.DecodeCurrentFrame();
+
+        if (result != null)
+        {
+            Debug.Log(result);
+        }
+    }
+}
